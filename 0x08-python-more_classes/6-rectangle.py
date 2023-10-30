7ab8f57663114953174eba8da58085c95ded50c2
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-10-30 14:32:58 -0700

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,6 +16,8 @@ class Rectangle:
     Public class attribute number_of_instances
     """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initialization method """
         Rectangle.number_of_instances += 1
