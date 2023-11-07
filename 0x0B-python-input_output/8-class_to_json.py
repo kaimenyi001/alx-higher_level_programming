2f734d9192858d59c4a003b2f762c1cd05047014
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-11-07 13:55:07 -0800

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
+
+    my_dict = {}
+    if hasattr(obj, "__dict__"):
+        my_dict = obj.__dict__.copy()
+    return my_dict
