9b1ea631e81f624d81da36defeda14b58bd88cb7
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-11-07 12:58:16 -0800

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that contains a function that appends to a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Function that appends a string to a text file
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
