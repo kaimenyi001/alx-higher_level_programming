a485d2816b6a10e303d1c2e75ab40320aea182a3
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-11-06 23:13:15 -0800

    class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True/False if obj is a type of a_class
+    """
+    return type(obj) is a_class
