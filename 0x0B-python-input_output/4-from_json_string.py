c4c4dc4b7ad1898a3fa17834aa8fb5fa5239821e
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-11-07 13:16:38 -0800

    from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that contains a function that returns an object
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that returns an object a string JSON representation
+    """
+    return json.loads(my_str)
