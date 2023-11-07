a8da6b004cffc4f51d41cdd5df49ffdf5d898b19
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-11-07 14:32:59 -0800

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student
+"""
+
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Intialization method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+
+        if isinstance(attrs, list) and all(isinstance(a, str)
+                                           for a in attrs):
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+        return self.__dict__
