1f1d6a11f6dc14ae8fcc564aea534b1e6430fac8
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-12-14 18:09:29 -0800

    mysql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Script that creates a database and a table
+-- Query to create database hbtn_0d_usa
+-- Query to create states table with 2 constraints
+CREATE DATABASE
+IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
