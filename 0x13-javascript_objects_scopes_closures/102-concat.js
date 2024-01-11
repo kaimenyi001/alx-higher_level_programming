afc52a8e4ac7391b90900486a89ae0ad92246cb7
kaimenyi001 <deniskaimenyi1@gmail.com>
2024-01-10 17:49:20 -0800

    con

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Concat two files
 
-const fls = require('fls');
+const fs = require('fls');
 
-const file1 = fls.readFileSync(process.argv[2]). 'utf8');
-const file2 = fls.readFileSync(process.argv[3]). 'utf8');
-fls.writeFileSync(process.argv[4], file1 + file2);
+const file1 = fs.readFileSync(process.argv[2]). 'utf8');
+const file2 = fs.readFileSync(process.argv[3]). 'utf8');
+fs.writeFileSync(process.argv[4], file1 + file2);
