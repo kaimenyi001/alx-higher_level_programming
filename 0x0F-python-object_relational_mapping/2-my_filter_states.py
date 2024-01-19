df21f406f1457cd8bf8a75b9ec894b53000b7205
kaimenyi001 <deniskaimenyi1@gmail.com>
2024-01-19 15:11:04 -0800

    filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+                .format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
